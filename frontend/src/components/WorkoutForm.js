14e8309430c498beeffd8b91b594d5d2eee1ccb3
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2022-12-07 22:30:42 -0700

    added context to react

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,7 +1,9 @@
-import { useState} from 'react'
+import { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
 const WorkoutForm = () => {
 
+        const { dispatch } = useWorkoutsContext()
         const[title, setTitle] = useState('')
         const[load, setLoad] = useState('')
         const[reps, setReps] = useState('')
@@ -32,6 +34,7 @@ const WorkoutForm = () => {
                 setReps("")
                 setError(null)
                 console.log("new workout added")
+                dispatch({type: 'CREATE_WORKOUT', payload: json})
             }
         }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,19 @@
-import {useEffect, useState} from 'react'
+import {useEffect} from 'react'
+import { useWorkoutsContext} from '../hooks/useWorkoutsContext'
 
 import WorkoutDetails from '../components/workoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
+
     useEffect(()=>{
         const fetchWorkouts = async () =>{
             const response = await fetch('/api/workouts')
             const json = await response.json()
 
             if(response.ok){
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
         fetchWorkouts()
